3420161d2ab4960ad4bc048bc7284c8e2036ebb3
UIProf <cis.roshan27ag@gmail.com>
2020-07-24 14:44:00 +1000

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-growl'
+import { useGrowl, Growl } from 'react-growl'
 import 'react-growl/dist/index.css'
+import './App.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [active, setActive] = useGrowl()
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a className="App-link" href="/sample">
+          Go to the Other component
+        		</a>
+        <p>
+            Updated App.js file
+        		</p>
+
+        <a className="App-link" href="#" onClick={() => void setActive(true)}>
+          Clik here to activate the growl
+        		</a>
+      </header>
+      <Growl onDismissed={() => setActive(false)} active={active} message="Hello World!" />
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+import './growl.css'
+
+export const Growl = ({ active, message, onDismissed }) => {
+  const [state, setState] = React.useState(true)
+
+  React.useEffect(() => {
+    setState(true)
+  }, [active])
+
+  const handleClick = () => {
+    setState(false)
+    onDismissed()
+  }
+
+  return (
+    <div className={`_growl__growl__1A4mq${active && state ? ' _growl__active__1Ykby' : ''}`}>
+      {message}
+      <div onClick={handleClick} className="_growl__growl-close__1ZbKB"></div>
+    </div>
+  )
+}
+
+export function useGrowl() {
+  // state of the growl
+  const [growlActive, setGrowlActive] = React.useState(false)
+  const onDismissed = () => {
+    setGrowlActive(false);
+  }
+  return [
+    // the first arg is the state
+    growlActive,
+
+    // the second arg is a fn that allows you to safely set its state
+    (active) => {
+      setGrowlActive(active);
+      setTimeout(onDismissed, 3000);
+    },
+  ]
 }
